7504a8cd4067269d54ed2acac8fe20a32ebc70fb
7eltantawy <hasaneltantawy@gmail.com>
2024-12-20 10:46:41 +0200

    utils to create search text column

---
+++ utils/search_text/main.py
@@ -0,0 +1,22 @@
+import sqlite3
+import re
+
+# Function to remove Arabic diacritics
+def remove_diacritics(text):
+    arabic_diacritics = re.compile(r'[\u0610-\u061A\u064B-\u065F\u0670]')
+    return arabic_diacritics.sub('', text)
+
+# Connect to the database
+conn = sqlite3.connect('book.db')
+
+# Register the function
+conn.create_function("REMOVE_DIACRITICS", 1, remove_diacritics)
+
+# Update the search_text column
+cursor = conn.cursor()
+cursor.execute("""
+    UPDATE titles
+    SET search_text = REMOVE_DIACRITICS(name)
+""")
+conn.commit()
+conn.close()
