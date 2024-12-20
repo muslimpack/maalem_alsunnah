abddabf76bfb91f31dcb2248772ca8485a2e8502
7eltantawy <hasaneltantawy@gmail.com>
2024-12-20 20:48:12 +0200

    Update content_viewer_screen.dart

--- maalem_alsunnah/lib/src/features/content_viewer/presentation/screens/content_viewer_screen.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/screens/content_viewer_screen.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:maalem_alsunnah/src/core/di/dependency_injection.dart';
 import 'package:maalem_alsunnah/src/features/search/data/models/content_model.dart';
 import 'package:maalem_alsunnah/src/features/search/data/models/title_model.dart';
 import 'package:maalem_alsunnah/src/features/search/data/repository/hadith_db_helper.dart';
+import 'package:maalem_alsunnah/src/features/settings/presentation/components/font_settings_widgets.dart';
+import 'package:maalem_alsunnah/src/features/settings/presentation/controller/cubit/settings_cubit.dart';
 
 class ContentViewerScreen extends StatefulWidget {
   final TitleModel title;
@@ -39,14 +42,22 @@ class _ContentViewerScreenState extends State<ContentViewerScreen> {
       appBar: AppBar(
         title: Text(widget.title.name),
         centerTitle: true,
+        actions: [FontSettingsIconButton()],
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : ListView(
               padding: EdgeInsets.all(15),
               children: [
-                Text(
-                  content.text,
+                SelectableText(
+                  context.watch<SettingsCubit>().state.showDiacritics
+                      ? content.text
+                      : content.searchText,
+                  style: TextStyle(
+                    fontSize:
+                        context.watch<SettingsCubit>().state.fontSize * 10,
+                    fontFamily: 'kitab',
+                  ),
                 ),
               ],
             ),
