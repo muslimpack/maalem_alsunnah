fdea5040b9c91c27736f0e5b16b548e2326d72e2
7eltantawy <hasaneltantawy@gmail.com>
2024-12-20 20:17:07 +0200

    Create title_card.dart

---
+++ maalem_alsunnah/lib/src/features/home/presentation/components/title_card.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:flutter/material.dart';
+import 'package:maalem_alsunnah/src/core/extensions/extension.dart';
+import 'package:maalem_alsunnah/src/features/content_viewer/presentation/screens/content_viewer_screen.dart';
+import 'package:maalem_alsunnah/src/features/content_viewer/presentation/screens/sub_titles_viewer_screen.dart';
+import 'package:maalem_alsunnah/src/features/search/data/models/title_model.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class TitleCard extends StatelessWidget {
+  const TitleCard({
+    super.key,
+    required this.title,
+  });
+
+  final TitleModel title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      child: ListTile(
+        onTap: () {
+          if (title.subTitlesCount == 0) {
+            context.push(ContentViewerScreen(title: title));
+          } else {
+            context.push(SubTitlesViewerScreen(title: title));
+          }
+        },
+        leading: Icon(
+          MdiIcons.bookOpenPageVariant,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        title: Text(title.name),
+        subtitle: Text("عدد الفصول: ${title.subTitlesCount}"),
+        trailing: Icon(Icons.chevron_right_outlined),
+      ),
+    );
+  }
+}
