aeb34fba0fffda8ec44d0a7ec71b4c515379b8d7
7eltantawy <hasaneltantawy@gmail.com>
2024-12-20 20:17:12 +0200

    Create content_viewer_screen.dart

---
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/screens/content_viewer_screen.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:maalem_alsunnah/src/core/di/dependency_injection.dart';
+import 'package:maalem_alsunnah/src/features/search/data/models/content_model.dart';
+import 'package:maalem_alsunnah/src/features/search/data/models/title_model.dart';
+import 'package:maalem_alsunnah/src/features/search/data/repository/hadith_db_helper.dart';
+
+class ContentViewerScreen extends StatefulWidget {
+  final TitleModel title;
+  const ContentViewerScreen({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<ContentViewerScreen> createState() => _ContentViewerScreenState();
+}
+
+class _ContentViewerScreenState extends State<ContentViewerScreen> {
+  bool isLoading = true;
+  late final ContentModel content;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future init() async {
+    content = await sl<HadithDbHelper>().getContentByTitleId(widget.title.id);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title.name),
+        centerTitle: true,
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView(
+              padding: EdgeInsets.all(15),
+              children: [
+                Text(
+                  content.text,
+                ),
+              ],
+            ),
+    );
+  }
+}
