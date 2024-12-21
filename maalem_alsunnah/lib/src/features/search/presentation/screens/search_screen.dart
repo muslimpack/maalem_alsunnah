39b3d0b999fe6cc67a80cce7c39f5b305c8a454e
7eltantawy <hasaneltantawy@gmail.com>
2024-12-21 15:11:01 +0200

    Update search_screen.dart

--- maalem_alsunnah/lib/src/features/search/presentation/screens/search_screen.dart
+++ maalem_alsunnah/lib/src/features/search/presentation/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maalem_alsunnah/src/features/search/presentation/components/search_for_bar.dart';
 import 'package:maalem_alsunnah/src/features/search/presentation/components/search_result_viewer.dart';
 import 'package:maalem_alsunnah/src/features/search/presentation/components/search_type_bar.dart';
 import 'package:maalem_alsunnah/src/features/search/presentation/controller/cubit/search_cubit.dart';
@@ -17,8 +18,10 @@ class SearchScreen extends StatelessWidget {
 
         return Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 15,
           children: [
             const SizedBox(height: 15),
+            const SearchForBar(),
             const SearchTypeBar(),
             const Expanded(
               child: SearchResultViewer(),
