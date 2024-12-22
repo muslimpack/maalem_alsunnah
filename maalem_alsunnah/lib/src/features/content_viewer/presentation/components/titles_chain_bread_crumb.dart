92bab5c551c2710a1ebccbd64c6db689c0b9d064
7eltantawy <hasaneltantawy@gmail.com>
2024-12-22 17:48:07 +0200

    Create and use TitlesChainRichTextBuilder

--- maalem_alsunnah/lib/src/features/content_viewer/presentation/components/titles_chain_bread_crumb.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/components/titles_chain_bread_crumb.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
 import 'package:maalem_alsunnah/src/core/di/dependency_injection.dart';
@@ -64,9 +65,83 @@ class _TitlesChainBreadCrumbState extends State<TitlesChainBreadCrumb> {
             ),
           );
         } else {
-          return TitlesChainBreadCrumbBuilder(titlesChains: snapshot.data!);
+          return TitlesChainRichTextBuilder(titlesChains: snapshot.data!);
         }
       },
+    );
+  }
+}
+
+class TitlesChainRichTextBuilder extends StatelessWidget {
+  const TitlesChainRichTextBuilder({
+    super.key,
+    required this.titlesChains,
+  });
+
+  final List<TitleModel> titlesChains;
+
+  void onPressed(BuildContext context, int index, TitleModel title) {
+    bool titleFoundInStack = false;
+    for (var route in routeStack) {
+      final int? routeTitleId = route.settings.arguments as int?;
+      if (routeTitleId != null && title.id == routeTitleId) {
+        titleFoundInStack = true;
+        break;
+      }
+      appPrint(title);
+    }
+    if (titleFoundInStack) {
+      Navigator.popUntil(
+          context, (r) => (r.settings.arguments as int) == title.id);
+    } else {
+      if (routeStack.length <= 1) {
+        context.pushNamed(
+          ContentViewerScreen.routeName,
+          arguments: title.id,
+        );
+      } else {
+        Navigator.of(context).pushReplacementNamed(
+          ContentViewerScreen.routeName,
+          arguments: title.id,
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<InlineSpan> spans = titlesChains.map((title) {
+      return TextSpan(
+        text: title.name,
+        style: TextStyle(
+          fontSize: 15,
+          color: Theme.of(context).colorScheme.secondary,
+          fontWeight: FontWeight.bold,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap =
+              () => onPressed(context, titlesChains.indexOf(title), title),
+      );
+    }).toList();
+
+    // The item to insert between the spans
+    final InlineSpan separator = WidgetSpan(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Text("/"),
+    ));
+
+// Interleave the separator between the spans
+    final List<InlineSpan> interleavedSpans = spans.expand((span) sync* {
+      yield span;
+      if (span != spans.last) {
+        yield separator;
+      }
+    }).toList();
+
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Text.rich(TextSpan(children: interleavedSpans)),
     );
   }
 }
