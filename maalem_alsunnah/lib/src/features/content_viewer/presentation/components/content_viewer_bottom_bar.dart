40d016ddf14f920e4c03d74757a518748da438d6
7eltantawy <hasaneltantawy@gmail.com>
2024-12-22 13:10:17 +0200

    move ContentViewerBottomBar  to separate file

---
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/components/content_viewer_bottom_bar.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/data/models/bookmark_type.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/add_note_button.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/bookmark_button.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/mark_as_read_button.dart';
+import 'package:maalem_alsunnah/src/features/content_viewer/presentation/controller/cubit/content_viewer_cubit.dart';
+import 'package:maalem_alsunnah/src/features/settings/presentation/components/font_settings_widgets.dart';
+
+class ContentViewerBottomBar extends StatelessWidget {
+  const ContentViewerBottomBar({
+    super.key,
+    required this.state,
+  });
+
+  final ContentViewerLoadedState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(
+          onPressed: !state.hasPrevious
+              ? null
+              : () {
+                  context.read<ContentViewerCubit>().previousContent();
+                },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        FontSettingsIconButton(),
+        BookmarkButton(
+          itemId: state.content.titleId,
+          type: BookmarkType.title,
+        ),
+        MarkAsReadButton(
+          itemId: state.content.titleId,
+          type: BookmarkType.title,
+        ),
+        AddNoteButton(
+          itemId: state.content.titleId,
+          type: BookmarkType.title,
+        ),
+        IconButton(
+          onPressed: !state.hasNext
+              ? null
+              : () {
+                  context.read<ContentViewerCubit>().nextContent();
+                },
+          icon: Icon(Icons.arrow_forward_ios),
+        ),
+      ],
+    ));
+  }
+}
--- maalem_alsunnah/lib/src/features/content_viewer/presentation/screens/content_screen.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/screens/content_screen.dart
@@ -2,13 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/data/models/bookmark_type.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/add_note_button.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/bookmark_button.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/mark_as_read_button.dart';
+import 'package:maalem_alsunnah/src/features/content_viewer/presentation/components/content_viewer_bottom_bar.dart';
 import 'package:maalem_alsunnah/src/features/content_viewer/presentation/components/titles_chain_bread_crumb.dart';
 import 'package:maalem_alsunnah/src/features/content_viewer/presentation/controller/cubit/content_viewer_cubit.dart';
-import 'package:maalem_alsunnah/src/features/settings/presentation/components/font_settings_widgets.dart';
 import 'package:maalem_alsunnah/src/features/settings/presentation/controller/cubit/settings_cubit.dart';
 
 class ContentScreen extends StatelessWidget {
@@ -47,41 +43,7 @@ class ContentScreen extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-            onPressed: !state.hasPrevious
-                ? null
-                : () {
-                    context.read<ContentViewerCubit>().previousContent();
-                  },
-            icon: Icon(Icons.arrow_back_ios),
-          ),
-          FontSettingsIconButton(),
-          BookmarkButton(
-            itemId: state.content.titleId,
-            type: BookmarkType.title,
-          ),
-          MarkAsReadButton(
-            itemId: state.content.titleId,
-            type: BookmarkType.title,
-          ),
-          AddNoteButton(
-            itemId: state.content.titleId,
-            type: BookmarkType.title,
-          ),
-          IconButton(
-            onPressed: !state.hasNext
-                ? null
-                : () {
-                    context.read<ContentViewerCubit>().nextContent();
-                  },
-            icon: Icon(Icons.arrow_forward_ios),
-          ),
-        ],
-      )),
+      bottomNavigationBar: ContentViewerBottomBar(state: state),
     );
   }
 }
