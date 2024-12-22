1195d887cd3349367eb5ca8c0fa839fd41f3acdb
7eltantawy <hasaneltantawy@gmail.com>
2024-12-22 08:30:57 +0200

    Create bookmark_button.dart

---
+++ maalem_alsunnah/lib/src/features/bookmarks/presentation/components/bookmark_button.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/data/models/bookmark_type.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/presentation/controller/bloc/bookmarks_bloc.dart';
+
+class BookmarkButton extends StatelessWidget {
+  final int itemId;
+  final BookmarkType type;
+  const BookmarkButton({
+    super.key,
+    required this.itemId,
+    required this.type,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BookmarksBloc, BookmarksState>(
+      builder: (context, state) {
+        if (state is! BookmarksLoadedState) {
+          return SizedBox();
+        }
+        final isBookmarked = state.bookmarks.any((element) =>
+            element.itemId == itemId &&
+            element.type == type &&
+            element.isBookmarked);
+        if (isBookmarked) {
+          return IconButton(
+            onPressed: () {
+              context.read<BookmarksBloc>().add(
+                    BookmarksBookmarkItemEvent(
+                      itemId: itemId,
+                      type: type,
+                      isBookmarked: false,
+                    ),
+                  );
+            },
+            icon: Icon(Icons.bookmark_outlined),
+          );
+        }
+        return IconButton(
+          onPressed: () {
+            context.read<BookmarksBloc>().add(
+                  BookmarksBookmarkItemEvent(
+                    itemId: itemId,
+                    type: type,
+                    isBookmarked: true,
+                  ),
+                );
+          },
+          icon: Icon(Icons.bookmark_border_outlined),
+        );
+      },
+    );
+  }
+}
