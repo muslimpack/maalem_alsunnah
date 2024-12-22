cacbef4b491d44e3947e5c0f33d37bde467b2d83
7eltantawy <hasaneltantawy@gmail.com>
2024-12-22 13:39:36 +0200

    fix: nav between content cause show subList when title has subTitles

--- maalem_alsunnah/lib/src/features/content_viewer/presentation/controller/cubit/content_viewer_cubit.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/controller/cubit/content_viewer_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:maalem_alsunnah/src/core/functions/print.dart';
 import 'package:maalem_alsunnah/src/features/home/presentation/controller/cubit/home_cubit.dart';
 import 'package:maalem_alsunnah/src/features/search/data/models/content_model.dart';
 import 'package:maalem_alsunnah/src/features/search/data/models/title_model.dart';
@@ -25,11 +26,11 @@ class ContentViewerCubit extends Cubit<ContentViewerState> {
     homeCubit.updateLastReadTitle(state.content.titleId);
   }
 
-  Future<void> start(int titleId) async {
+  Future<void> start(int titleId, {bool? isContent}) async {
     _timer?.cancel();
 
     final title = (await hadithDbHelper.getTitleById(titleId))!;
-    if (title.subTitlesCount == 0) {
+    if (title.subTitlesCount == 0 || isContent == true) {
       _timer = Timer.periodic(const Duration(seconds: 5), _timePassed);
       final content = await hadithDbHelper.getContentByTitleId(titleId);
       final contentCount = await hadithDbHelper.getContentCount();
@@ -68,7 +69,8 @@ class ContentViewerCubit extends Cubit<ContentViewerState> {
     if (contentId > 1 && contentId < state.contentCount) {
       final ContentModel nextContent =
           await hadithDbHelper.getContentById(contentId + by);
-      start(nextContent.titleId);
+      appPrint(nextContent.titleId);
+      start(nextContent.titleId, isContent: true);
     }
   }
 
