ddcde71d9552de5a483bec2520025583af30007e
7eltantawy <hasaneltantawy@gmail.com>
2024-12-26 06:55:05 +0200

    Update text_formatter_item.dart

--- maalem_alsunnah/lib/src/features/content_viewer/data/models/text_formatter_item.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/data/models/text_formatter_item.dart
@@ -1,15 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:flutter/material.dart';
+import 'package:maalem_alsunnah/src/features/content_viewer/data/models/format_type_enum.dart';
 
 class TextFormatterItem {
-  final String name;
+  final FormatTypeEnum formatType;
   final RegExp regExp;
   final TextStyle textStyle;
   final bool Function(String text) predicate;
 
   TextFormatterItem({
-    required this.name,
+    required this.formatType,
     required this.regExp,
     required this.textStyle,
     bool Function(String text)? predicate,
