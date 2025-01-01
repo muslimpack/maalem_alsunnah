53e450c6241b80bf95a72ced8e010c5902f2f1ae
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-01-01 16:38:59 +0200

    Update content_viewer_bottom_bar.dart

--- maalem_alsunnah/lib/src/features/content_viewer/presentation/components/content_viewer_bottom_bar.dart
+++ maalem_alsunnah/lib/src/features/content_viewer/presentation/components/content_viewer_bottom_bar.dart
@@ -1,15 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:flutter/material.dart';
-import 'package:maalem_alsunnah/generated/l10n.dart';
 import 'package:maalem_alsunnah/src/features/bookmarks/data/models/bookmark_type.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/add_note_button.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/bookmark_button.dart';
-import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/mark_as_read_button.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/presentation/components/bookmark_action_bar.dart';
 import 'package:maalem_alsunnah/src/features/content_viewer/presentation/components/content_nav_bar.dart';
 import 'package:maalem_alsunnah/src/features/content_viewer/presentation/controller/cubit/content_viewer_cubit.dart';
 import 'package:maalem_alsunnah/src/features/share/data/models/share_type.dart';
-import 'package:maalem_alsunnah/src/features/share/presentation/components/share_dialog.dart';
 
 class ContentViewerBottomBar extends StatelessWidget {
   const ContentViewerBottomBar({
@@ -26,34 +22,12 @@ class ContentViewerBottomBar extends StatelessWidget {
       children: [
         ContentNavBar(state: state),
         BottomAppBar(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              tooltip: S.of(context).share,
-              onPressed: () {
-                showShareDialog(
-                  context,
-                  itemId: state.content.id.toString(),
-                  shareType: ShareType.content,
-                );
-              },
-              icon: Icon(Icons.share),
-            ),
-            BookmarkButton(
-              itemId: state.content.titleId.toString(),
-              type: BookmarkType.title,
-            ),
-            MarkAsReadButton(
-              itemId: state.content.titleId.toString(),
-              type: BookmarkType.title,
-            ),
-            AddNoteButton(
-              itemId: state.content.titleId.toString(),
-              type: BookmarkType.title,
-            ),
-          ],
-        )),
+          child: BookmarkActionBar(
+            itemId: state.content.id.toString(),
+            bookmarkType: BookmarkType.title,
+            shareType: ShareType.content,
+          ),
+        ),
       ],
     );
   }
