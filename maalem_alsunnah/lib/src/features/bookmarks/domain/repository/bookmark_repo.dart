72d1922ad95f726fa9ccb49949861da1ad578981
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-01-01 20:20:19 +0200

    Create bookmark_repo.dart

---
+++ maalem_alsunnah/lib/src/features/bookmarks/domain/repository/bookmark_repo.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+import 'package:maalem_alsunnah/src/features/bookmarks/data/models/bookmark_view_enum.dart';
+
+class BookmarkRepo {
+  final Box box;
+
+  BookmarkRepo(this.box);
+
+  /// Search Type
+  static const String bookmarkViewTypeKey = "bookmarkView";
+  BookmarkViewEnum get bookmarkView {
+    final data = box.get(bookmarkViewTypeKey) as String?;
+    if (data == null) return BookmarkViewEnum.titles;
+    return BookmarkViewEnum.fromString(data);
+  }
+
+  Future setSearchType(BookmarkViewEnum bookmarkView) async {
+    return box.put(bookmarkViewTypeKey, bookmarkView.name);
+  }
+}
