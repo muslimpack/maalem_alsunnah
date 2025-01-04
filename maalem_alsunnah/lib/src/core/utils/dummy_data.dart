6765069b4475154c01281aa1f012e90dc5a6d8c6
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-01-04 19:33:24 +0200

    Update dummy_data.dart

--- maalem_alsunnah/lib/src/core/utils/dummy_data.dart
+++ maalem_alsunnah/lib/src/core/utils/dummy_data.dart
@@ -1,3 +1,5 @@
+import 'package:maalem_alsunnah/src/features/search/data/models/content_model.dart';
+import 'package:maalem_alsunnah/src/features/search/data/models/hadith_model.dart';
 import 'package:maalem_alsunnah/src/features/search/data/models/title_model.dart';
 
 final titleModelDummyData = List.generate(
@@ -5,8 +7,41 @@ final titleModelDummyData = List.generate(
   (index) => TitleModel(
     id: index + 1,
     orderId: (index + 1) * index,
-    name: 'Title $index',
+    name: 'lorem ipsum dolor $index' * index,
     parentId: -1,
     subTitlesCount: 0,
   ),
+);
+
+final contentModelDummyData = List.generate(
+  10,
+  (index) {
+    final String text = "lorem ipsum dolor" * (index + 5) * index;
+    return ContentModel(
+      id: index + 1,
+      orderId: (index + 1) * index,
+      searchText: text,
+      text: text,
+      titleId: index + 1,
+    );
+  },
+);
+
+final hadithModelDummyData = List.generate(
+  10,
+  (index) {
+    final String text =
+        List.generate((index + 15) * index, (index) => "lorem ipsum dolor")
+            .join(" ")
+            .toString();
+    return HadithModel(
+      id: "${index + 1}",
+      count: index + index,
+      contentId: index + 1,
+      orderId: (index + 1) * index,
+      searchText: text,
+      text: text,
+      titleId: index + 1,
+    );
+  },
 );
