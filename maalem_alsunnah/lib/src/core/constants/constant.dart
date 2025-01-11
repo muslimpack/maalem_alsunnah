a408b642b14d303d042cbbcf76bcb75687197f6c
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-01-11 03:30:44 +0200

    update app version

--- maalem_alsunnah/lib/src/core/constants/constant.dart
+++ maalem_alsunnah/lib/src/core/constants/constant.dart
@@ -1,6 +1,6 @@
-part 'string_constant.dart';
-part 'links_constants.dart';
 part 'font_constants.dart';
+part 'links_constants.dart';
+part 'string_constant.dart';
 
 const String kHiveBoxName = "maalem_alsunnah";
-const String kAppVersion = "0.1.0";
+const String kAppVersion = "0.2.0";
